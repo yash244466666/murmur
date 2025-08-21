18a72d9f132a1821a5c869b6329c879fa4ebe40c
yash244466666 <info@yashcodes.com>
2025-08-22 00:09:34 +0600

    dockerize the rails part

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,11 @@
-//= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../builds
+//= link_tree ../images
 //= link application.js
+
+// Asset manifest for Sprockets
+// This file tells Sprockets which assets to include in the Rails asset pipeline
+// - stylesheets: CSS files
+// - builds: Compiled assets (like Tailwind CSS)
+// - images: Image assets
+// - application.js: Main JavaScript entry point
